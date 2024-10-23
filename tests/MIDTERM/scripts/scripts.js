b216e2233562c4566592464b149775667219ee11
Jonathan Bauer <97413100+Jonathan-Bauer9001@users.noreply.github.com>
2024-10-23 12:12:29 -0500

    Midterm update redid styling and added js for navbar color switching

--- tests/MIDTERM/scripts/scripts.js
+++ tests/MIDTERM/scripts/scripts.js
@@ -4,3 +4,21 @@ const myToast = new bootstrap.Toast('.toast');
 setTimeout(() => {
   myToast.show();
 }, 1000);
+
+//Scroll Navbar Color Switch (easier to read navbar over backgrounds)
+const header = document.querySelector('#header');
+const hero = document.querySelector('.hero');
+
+const heroOptions = {};
+
+const heroObserver = new IntersectionObserver(function(entries, heroObserver) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      header.classList.add('nav-scrolled');
+    } else {
+      header.classList.remove('nav-scrolled');
+    }
+  });
+}, heroOptions);
+
+heroObserver.observe(hero);
